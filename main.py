3e1b155f82e0d94971a52f60c6cbf55f18fc8f6f
unknown <anton-qz@yandex.ru>
2022-11-11 16:10:08 +0700

    dir+rm

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import urllib.request
+import os
+import shutil
 from PIL import Image
 
 
 images = []
-N = 195  # pages number
+N = 5  # pages number
 link = 'https://e-lib.nsu.ru/reader/service/SecureViewer/Page/UmVzb3VyY2UtMTAwNg/cGFnZTAwMDAw/'
+dir_name = link[-13:-1]
+os.mkdir(dir_name)
 for i in range(0, N):
     urllib.request.urlretrieve(
         f'{link}{i}',
-        f"data/{i}.png")
-    images.append(Image.open(f"data/{i}.png"))
+        f"{dir_name}/{i}.png")
+    images.append(Image.open(f"{dir_name}/{i}.png"))
 images[0].save(
     "out.pdf", "PDF", resolution=100.0, save_all=True, append_images=images[1:]
 )
+shutil.rmtree(dir_name)
